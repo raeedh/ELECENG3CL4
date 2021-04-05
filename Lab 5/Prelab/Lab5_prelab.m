4a2ca7234745fe22d69ffa028cac715c99667ebc
Aaron <10177767+AaronPinto@users.noreply.github.com>
2021-04-05 10:10:12 -0400

    updated pre-lab script

--- Lab 5/Prelab/Lab5_prelab.m
+++ Lab 5/Prelab/Lab5_prelab.m
@@ -1,5 +1,4 @@
- 
-clear all; close all;
+clear variables; clear global; close all;
 
 %Prelab Plant
 G=tf(4.7,[1,3.2,0]);
@@ -7,7 +6,7 @@ G=tf(4.7,[1,3.2,0]);
 %Enter the lead controller you designed in the prelab for Lab 4
 %For reference, I have included here the lead controller that I designed.
 %However, you should enter the one that you designed.
-%Gc_lead=tf(51.39*[1,5.333],[1,13.266]);
+Gc_lead=tf(62.1*[1,6],[1,15.51]);
 
 %Select a position for the zero of the lag compensator and then calculate
 %the desired position of the pole
@@ -15,7 +14,6 @@ G=tf(4.7,[1,3.2,0]);
 %fit
 zlag=0.2;
 plag=zlag/5;
-
 
 %Construct the transfer function model of the lead-lag compensator
 lag_system=tf([1,zlag],[1,plag]);
@@ -38,41 +36,45 @@ T_leadlag=feedback(L_leadlag,1);
 cl_poles_lead=pole(T_lead)
 cl_poles_leadlag=pole(T_leadlag)
 
+phase0 = rad2deg(atan2(imag(cl_poles_leadlag(1)) - 0, real(cl_poles_leadlag(1)) - zlag));
+phase1 = rad2deg(atan2(imag(cl_poles_leadlag(1)) - 0, real(cl_poles_leadlag(1)) - plag));
+phase_delta = phase0 - phase1
+
 %Plot the closed loop pole positions. Lead with square, lead-lag with
 %triangle
-plot(real(cl_poles_lead),imag(cl_poles_lead),'s');
-plot(real(cl_poles_leadlag),imag(cl_poles_leadlag),'^')
-
-%Calculate the step responses
-figure
-step(T_lead)
-hold on
-step(T_leadlag)
-
-%Calculate the ramp responses
-figure
-[maxpole,index_maxpole]=max(real(cl_poles_leadlag));
-Tmax=4/abs(maxpole);
-tvec=0:0.01:Tmax;
-input_vec=tvec;
-lsim(T_lead,input_vec,tvec)
-hold on
-lsim(T_leadlag,input_vec,tvec)
-
-%Draw the Bode diagrams
-figure
-bode(L_lead)
-hold on
-bode(L_leadlag)
-grid
-
-%Construct the closed-loop transfer function from disturbances to output
-disturbance_TF_lead = feedback(G,Gc_lead);
-disturbance_TF_leadlag=feedback(G,Gc_leadlag);
-
-%Calculate disturbance transfer functions
-figure
-step(disturbance_TF_lead,50);
-hold on
-step(disturbance_TF_leadlag,50);
-grid
+% plot(real(cl_poles_lead),imag(cl_poles_lead),'s');
+% plot(real(cl_poles_leadlag),imag(cl_poles_leadlag),'^')
+% 
+% %Calculate the step responses
+% figure
+% step(T_lead)
+% hold on
+% step(T_leadlag)
+% 
+% %Calculate the ramp responses
+% figure
+% [maxpole,index_maxpole]=max(real(cl_poles_leadlag));
+% Tmax=4/abs(maxpole);
+% tvec=0:0.01:Tmax;
+% input_vec=tvec;
+% lsim(T_lead,input_vec,tvec)
+% hold on
+% lsim(T_leadlag,input_vec,tvec)
+% 
+% %Draw the Bode diagrams
+% figure
+% bode(L_lead)
+% hold on
+% bode(L_leadlag)
+% grid
+% 
+% %Construct the closed-loop transfer function from disturbances to output
+% disturbance_TF_lead = feedback(G,Gc_lead);
+% disturbance_TF_leadlag=feedback(G,Gc_leadlag);
+% 
+% %Calculate disturbance transfer functions
+% figure
+% step(disturbance_TF_lead,50);
+% hold on
+% step(disturbance_TF_leadlag,50);
+% grid
